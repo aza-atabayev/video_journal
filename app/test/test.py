8d55757bb0714314993df66a955eaec973f45e03
Azamat Atabayev <aza.atabayev@gmail.com>
2021-11-19 15:33:50 +0900

    implemented DAN on the server

--- app/test/test.py
+++ app/test/test.py
@@ -1,5 +1,5 @@
 import requests
 
-resp = requests.post("http://localhost:5000/predict", files={'file':open('seven.png', 'rb')})
+resp = requests.post("http://localhost:5000/predict?path=../data/pexels-mikhail-nilov-6706798.mp4")
 
 print(resp.text)
